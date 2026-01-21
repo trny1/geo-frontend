efd29fbab49f26ec0cf693f4137e0a86563fc24b
botondterenyi <botiterenyi@gmail.com>
2026-01-21 11:35:41 +0100

    fetch

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    (async function(){
+      try {
+      const gotMessageJson = await fetch('https://')
+      console.log(gotMessageJson)
+      const gotMessage = await gotMessageJson.json()
+      setMessage(gotMessage)
+    } catch (error) {
+      console.warn(error)
+    }
+    })()
+  }, [])
 
   const handleLatLongClick = async (e) => {
     if (navigator.geolocation) {
